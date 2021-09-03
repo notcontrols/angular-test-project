35b6cc668801eceb4eff41cb7111309177369a2b
Vladislav Moroz <UMaroz@ibagroup.eu>
2021-09-03 12:30:54 +0300

    Fix form

--- src/app/edit-role/edit-role.component.ts
+++ src/app/edit-role/edit-role.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-edit-role',
@@ -7,20 +10,52 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./edit-role.component.scss'],
 })
 export class EditRoleComponent implements OnInit {
-  ngForm = new FormGroup({
-    role: new FormControl(''),
-    desc: new FormControl(''),
-    phone: new FormControl(''),
-    email: new FormControl('', Validators.email),
-    password: new FormControl('', Validators.required),
-    selecttype: new FormControl(''),
-    check: new FormControl(''),
-  });
+  submitted = false;
 
-  constructor() {}
+  ngForm: FormGroup;
 
-  ngOnInit(): void {}
+  get password() { return this.ngForm.get('password'); }
+  get email() { return this.ngForm.get('email'); }
+  get testFormControl() {
+    return this.ngForm.controls;
+  }
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.ngForm = new FormGroup({
+      role: new FormControl('', Validators.required, this.userNameValidator),
+      desc: new FormControl(''),
+      phone: new FormControl('', Validators.pattern(/^\+375(25|29|33|44)[0-9]{3}[0-9]{2}[0-9]{2}$/)),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(8)]),
+      selecttype: new FormControl('Test'),
+      check: new FormControl(''),
+    });
+  }
   onSubmit() {
-    console.log(this.ngForm.value);
+    this.submitted = true;
+    if (this.ngForm.valid) { 
+      alert('Form Submitted succesfully!!!\n Check the values in browser console.');
+      console.table(this.ngForm.value);
+    }
+  }
+
+  onCancel() {
+    this.router.navigate(['/']);
+  }
+
+  userNameValidator(control: FormControl): Promise<any> | Observable<any> {
+    const UserList = ['Admin', 'User', 'Superuser'];
+
+    return new Promise<any>(resolve => {
+      setTimeout(() => {
+        if (UserList.indexOf( control.value ) != -1 ) {
+          resolve(null);
+        } else {
+          resolve({ userNameNotAvailable: true });
+        }
+      }, 1000);
+    });
   }
 }
